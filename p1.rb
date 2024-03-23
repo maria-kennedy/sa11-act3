fe78c1df1a7fc4f21eec71e32e294b034afd9e61
Maria Kennedy <mariakingsbury1@gmail.com>
2024-03-22 22:25:14 -0500

    p1 complete

--- p1.rb
+++ p1.rb
@@ -1,7 +1,12 @@
-
-# Example Code
 def extract_urls(text) 
-end 
+    pattern = /https?:\/*\w+.\w+.[^. ]*|ftp?:\/*\w+.\w+.[^. ]*/ 
+    url = text.scan(pattern)
+    puts url
+end
+
+
+ 
+
 sample_text = "Visit our site at http://www.example.org for more information. 
 Check out our search page at https://example.com/search?q=ruby+regex. 
 Don’t forget to ftp our resources at ftp://example.com/resources." 
